28ad931137ce959ea0f3b7c260801a7f82a3815f
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2022-06-18 19:43:09 +0200

    Desktop dirs css and imge, also desktop.pug for loop

--- src/system/api/routes/desktop/index.ts
+++ src/system/api/routes/desktop/index.ts
@@ -7,8 +7,30 @@ export default {
 
     get: async ({ res }) => {
 
+        // @todo Apps import from handler Map<>
+        const appImage = 'assets/images/desktop/defaultFile.png';
+        const apps = [
+            {
+                appName: 'settings', appImage, appEndpoint: '/settings'
+            },
+            {
+                appName: 'calc', appImage, appEndpoint: '/calc'
+            }
+        ];
+
+        // @todo Dirs manager / handler
+        const dirImage = 'assets/images/desktop/defaultDir.png';
+        const dirs = [
+            {
+                dirName: 'photos', dirImage, dirEndpoint: '/desktop/photos'
+            },
+            {
+                dirName: 'texts', dirImage, dirEndpoint: '/desktop/texts'
+            }
+        ];
+
         const l = lang.langs.get(lang.cache.language)?.desktop.home;
-        return res.render('desktop', { logOut: l?.logout, shutDown: l?.shutdown })
+        return res.render('desktop', { logOut: l?.logout, shutDown: l?.shutdown, apps, dirs })
 
     },
 

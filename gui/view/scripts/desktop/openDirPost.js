0f3c0b11d62058f6ef3715ba70ae3c2f58477035
Mateusz <46076250+mateuszkdev@users.noreply.github.com>
2022-06-20 11:26:31 +0200

    new ideas but big trouble

---
+++ gui/view/scripts/desktop/openDirPost.js
@@ -0,0 +1,10 @@
+const open = (path) => {
+
+    console.log(path);
+    // const res = await fetch('/dir', {
+    //     body: JSON.stringify({ path })
+    // });
+    //
+    // console.log(await res.json());
+
+}
